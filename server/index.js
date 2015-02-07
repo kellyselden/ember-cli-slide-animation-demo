1ae00de332ce49e9891fe0a6b8525aabe069b24d
Kelly Selden <kellyselden@gmail.com>
2015-02-07 10:22:32 -0500

    making route refreshes work

--- server/index.js
+++ server/index.js
@@ -4,4 +4,8 @@ var server = express();
 
 server.use(express.static(__dirname + '/public'));
 
+server.get('*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 server.listen(process.env.PORT || 3000);
